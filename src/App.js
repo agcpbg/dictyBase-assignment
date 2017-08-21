45f666e8708c3589b5c1443d8c9ab0def168dadb
Alex Cherubin <agcpbg@yahoo.com>
2017-08-21 14:12:25 -0500

    add prettier for code formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import store from './store'
-import PageWrapper from './styled-components/PageWrapper'
-import PageHeader from './styled-components/PageHeader'
-import InputContainer from './InputContainer'
-import ListContainer from './ListContainer'
+import React from "react"
+import { Provider } from "react-redux"
+import store from "./store"
+import PageWrapper from "./styled-components/PageWrapper"
+import PageHeader from "./styled-components/PageHeader"
+import InputContainer from "./InputContainer"
+import ListContainer from "./ListContainer"
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <PageWrapper>
-          <PageHeader>Manage my items</PageHeader>
-          <InputContainer />
-          <ListContainer />
-        </PageWrapper>
-      </Provider>
-    )
-  }
+const App = props => {
+  return (
+    <Provider store={store}>
+      <PageWrapper>
+        <PageHeader>Manage my items</PageHeader>
+        <InputContainer />
+        <ListContainer />
+      </PageWrapper>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
